c694aeb688abc52932e38d45a975188c141f77a8
Joaooh <83705635+Joaooh@users.noreply.github.com>
2026-05-22 11:48:47 -0300

    chore: atualiza configurações do projeto e SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://monitoria-remake.vercel.app",
+  site: "https://site-monitoria-blue.vercel.app",
   integrations: [react(), sitemap()],
 });
